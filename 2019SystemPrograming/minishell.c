87e5934dc15368b8851536261e90855318aabdc7
jshagold <jshagold@gmail.com>
2019-05-27 22:46:50 +0900

    working history function. Not Donegit add .git add .!

--- 2019SystemPrograming/minishell.c
+++ 2019SystemPrograming/minishell.c
@@ -11,7 +11,14 @@ int main(void) {
 	char *input, *command;
 	char cd_buf[255];
 	
-	chdir("/");
+	// initialize directory to user directory
+	char *username = getenv("USER");
+	char *host_dir;
+	sprintf(host_dir, "/home/%s", username);
+	chdir(host_dir);
+
+	// open history file
+	FILE *fp;
 
 	while(1) {
 		int i = 0;
@@ -32,19 +39,26 @@ int main(void) {
 			
 		}
 
-
+		// when input nothing 
 		if(args[0] == NULL){
 			goto no_input;
 		}
+		// when input exit
 		if(strcmp(args[0], "exit") == 0){
 			break;
 		}
+		// when input cd
 		else if(strcmp(args[0], "cd") == 0){
 			if(chdir(args[1])){
 				perror("error");
 				exit(1);
 			}
 		}
+		//when input history
+		else if(strcmp(args[0], "history") == 0){
+			
+		}
+		// when input ls
 		else if(strcmp(args[0], "ls") == 0){
 			if(fork() == 0) {
 				execv("/bin/ls",args);
